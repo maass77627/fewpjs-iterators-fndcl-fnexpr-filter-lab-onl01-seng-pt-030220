52825b63ffc0c1c32eb3612d905a3396ac35dc50
kelly r maass <kmaass77627@gmail.com>
2020-10-06 02:29:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// Code your solution here
-
-// function findMatching(drivers, name){
-//   return drivers.filter( find_name => {
-//     return (find_name.toLowerCase() === name.toLowerCase())
-//    }
-//   )
-// }
-
-
-
 
 function findMatching(drivers, name) {
 // let array = []
