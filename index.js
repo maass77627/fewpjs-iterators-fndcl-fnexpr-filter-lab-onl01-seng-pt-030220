b412a110ded5310d56efb583ea6761ceec24b98a
kelly r maass <kmaass77627@gmail.com>
2020-10-06 02:27:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Code your solution here
 
-function findMatching(drivers, name){
-  return drivers.filter( find_name => {
-    return (find_name.toLowerCase() === name.toLowerCase())
-   }
-  )
-}
+// function findMatching(drivers, name){
+//   return drivers.filter( find_name => {
+//     return (find_name.toLowerCase() === name.toLowerCase())
+//    }
+//   )
+// }
 
 
 
