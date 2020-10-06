3a7b07d3ed2079de9692220b38b2d71090cd6793
kelly r maass <kmaass77627@gmail.com>
2020-10-06 02:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+let array = []
+  let newDrivers = drivers.filter(driver => {
+    return driver === name
+  })
+}
+
+function fuzzyMatch(drivers, name) {
+
+}
+
+function matchName() {
+
+}
